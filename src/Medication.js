1434060a650ca269ed97fe8461475392b2ebaed9
diana011 <dianaelena011@gmail.com>
2018-04-16 13:56:15 +0300

    Update Medication.js

--- src/Medication.js
+++ src/Medication.js
@@ -68,11 +68,11 @@ class Medication extends Component {
         </table>
         <div class="pagination">
 
-          <a >1</a>
-          <a >2</a>
-          <a >3</a>
-          <a >4</a>
-          <a >5</a>
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
 
         </div>  
         </div>);
